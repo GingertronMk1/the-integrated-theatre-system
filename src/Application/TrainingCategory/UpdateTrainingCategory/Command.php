5c1725c52d6b1c1d851704aa4a7c8cad185810e7
Jack Ellis <jackellis1504@gmail.com>
2023-12-27 18:52:27 +0000

    styleed

--- src/Application/TrainingCategory/UpdateTrainingCategory/Command.php
+++ src/Application/TrainingCategory/UpdateTrainingCategory/Command.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Application\TrainingCategory\UpdateTrainingCategory;
 
 use App\Domain\TrainingCategory\TrainingCategoryEntity;
-use App\Domain\TrainingCategory\ValueObject\TrainingCategoryId;
 
 final class Command
 {
