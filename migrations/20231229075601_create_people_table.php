69f75c029af52d00f7ff05b153de207141a86c69
Jack Ellis <jackellis1504@gmail.com>
2023-12-29 08:01:49 +0000

    Add people table

---
+++ migrations/20231229075601_create_people_table.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreatePeopleTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     * Table people {
+  id string [primary key]
+  user_id string [null, ref: - users.id]
+  name string [null]
+  start_year string
+  end_year string
+  created_at timestamp
+  updated_at timestamp
+}
+
+     */
+    public function change(): void
+    {
+        $table = $this->table('people', ['id' => false, 'primary_key' => 'id']);
+        $table
+            ->addColumn('id', 'string')
+            ->addColumn('user_id', 'string', ['null' => true])
+            ->addColumn('name', 'string')
+            ->addColumn('start_year', 'string')
+            ->addColumn('end_year', 'string')
+            ->addColumn('created_at', 'string')
+            ->addColumn('updated_at', 'string')
+            ->addColumn('deleted_at', 'string')
+        ;
+
+        $table
+            ->addIndex('start_year')
+            ->addIndex('end_year')
+            ->addIndex('deleted_at')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'delete' => 'CASCADE',
+                    'update' => 'CASCADE'
+                ]
+            )
+            ->update()
+            ;
+    }
+}
