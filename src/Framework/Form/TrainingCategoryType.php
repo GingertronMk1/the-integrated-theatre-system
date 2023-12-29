ad165a5448742e17001561c7703ed73865949467
Jack Ellis <jackellis1504@gmail.com>
2023-12-29 06:33:45 +0000

    validation for the nation

--- src/Framework/Form/TrainingCategoryType.php
+++ src/Framework/Form/TrainingCategoryType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TrainingCategoryType extends AbstractType
 {
@@ -20,7 +21,10 @@ class TrainingCategoryType extends AbstractType
         $builder
             ->add(
                 'name',
-                TextType::class
+                TextType::class,
+                [
+                    'constraints' => [ new NotBlank() ]
+                ]
             )
             ->add(
                 'submit',
