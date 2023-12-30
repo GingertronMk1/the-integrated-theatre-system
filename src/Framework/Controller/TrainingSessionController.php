c07a3f33cfdb4e06b5e71c1a4b5e0e3b5aad0733
Jack Ellis <jackellis1504@gmail.com>
2023-12-30 19:48:00 +0000

    whoops missed one

--- src/Framework/Controller/TrainingSessionController.php
+++ src/Framework/Controller/TrainingSessionController.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Framework\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TrainingSessionController extends {c}{
-        public function __construct(
-            ) {}
+class TrainingSessionController extends AbstractController
+{
+    public function __construct(
+    ) {
     }
+}
