c71963eeddb4fdf4ec487d11368d5d9b79a40e1e
Jack Ellis <jackellis1504@gmail.com>
2023-12-30 16:48:57 +0000

    training controller test

--- tests/Framework/Controller/TrainingCategoryControllerTest.php
+++ tests/Framework/Controller/TrainingCategoryControllerTest.php
@@ -28,4 +28,41 @@ final class TrainingCategoryControllerTest extends UserInterfaceTest
         $categoryId = TrainingCategoryFixture::IDS[1];
         $this->assertSelectorExists("[data-category-id='{$categoryId}']");
     }
+
+    /**
+     * @test
+     */
+    public function testCreate(): void
+    {
+        $crawler = $this->client->request('GET', "/training-category/create");
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form[name=training_category]');
+        $newName = 'Second test category';
+        $form = $crawler->filter('form[name=training_category]')->form([
+          'training_category[name]' => $newName,
+        ]);
+        $crawler = $this->client->submit($form);
+        $this->assertStringEndsWith('/training-category', $crawler->getUri());
+        $this->assertSelectorTextContains('table#training-categories', $newName);
+    }
+
+    /**
+     * @test
+     */
+    public function testUpdate(): void
+    {
+        $categoryId = TrainingCategoryFixture::IDS[1];
+        $crawler = $this->client->request('GET', "/training-category/update/{$categoryId}");
+        $this->assertResponseIsSuccessful();
+        $this->assertStringEndsWith("/training-category/update/{$categoryId}", $crawler->getUri());
+        $this->assertSelectorExists('form[name=training_category]');
+        $newName = 'This should have updated';
+        $form = $crawler->filter('form[name=training_category]')->form([
+          'training_category[name]' => $newName,
+        ]);
+        $crawler = $this->client->submit($form);
+        $this->assertStringEndsWith('/training-category', $crawler->getUri());
+        $this->assertSelectorExists("[data-category-id='{$categoryId}']");
+        $this->assertSelectorTextContains("[data-category-id='{$categoryId}']", $newName);
+    }
 }
