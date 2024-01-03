7c5bb936e3493d90f763bb31e06e768eb19457af
Jack Ellis <jackellis1504@gmail.com>
2024-01-03 08:52:15 +0000

    Actually have methods

--- src/Infrastructure/Season/DbalSeasonFinder.php
+++ src/Infrastructure/Season/DbalSeasonFinder.php
@@ -5,12 +5,66 @@ declare(strict_types=1);
 namespace App\Infrastructure\Season;
 
 use App\Application\Season\SeasonFinderInterface;
+use App\Application\Season\SeasonModel;
+use App\Domain\Common\ValueObject\DateTime;
+use App\Domain\Season\SeasonException;
 use Doctrine\DBAL\Connection;
+use App\Domain\Season\ValueObject\SeasonId;
 
 final readonly class DbalSeasonFinder implements SeasonFinderInterface
 {
     public function __construct(
         private Connection $connection
     ) {
+    }
+
+    public function find(SeasonId $id): SeasonModel
+    {
+        $qb = $this->connection->createQueryBuilder();
+        $row = $qb
+            ->select('*')
+            ->from('seasons')
+            ->where('id = :id')
+            ->setParameter('id', (string) $id)
+            ->fetchAssociative();
+
+        if (!is_array($row)) {
+            throw new SeasonException("No row found with {$id}");
+        }
+
+        return $this->createFromRow($row);
+    }
+
+    public function findAll(): array
+    {
+        $qb = $this->connection->createQueryBuilder();
+        $rows = $qb
+            ->select('*')
+            ->from('seasons', 'tc')
+            ->fetchAllAssociative()
+        ;
+
+        return array_map(
+            fn (array $row) => $this->createFromRow($row),
+            $rows
+        );
+    }
+
+    private function createFromRow(array $row): SeasonModel
+    {
+        $deletedAt = null;
+        if (!is_null($row['deleted_at'])) {
+            $deletedAt = DateTime::fromString($row['deleted_at']);
+        }
+
+        return new SeasonModel(
+            SeasonId::fromString($row['id']),
+            $row['name'],
+            $row['description'],
+            $row['colour'],
+            DateTime::fromString($row['created_at']),
+            DateTime::fromString($row['updated_at']),
+            $deletedAt,
+        );
     }
 }
--- src/Infrastructure/Season/DbalSeasonRepository.php
+++ src/Infrastructure/Season/DbalSeasonRepository.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Infrastructure\Season;
 
 use App\Application\Common\Service\ClockInterface;
+use App\Domain\Season\SeasonEntity;
 use App\Domain\Season\SeasonRepositoryInterface;
+use App\Domain\Season\ValueObject\SeasonId;
 use Doctrine\DBAL\Connection;
 
 final readonly class DbalSeasonRepository implements SeasonRepositoryInterface
@@ -14,5 +16,20 @@ final readonly class DbalSeasonRepository implements SeasonRepositoryInterface
         private Connection $connection,
         private ClockInterface $clock
     ) {
+    }
+
+    public function getNextId(): SeasonId
+    {
+        return SeasonId::generate();
+    }
+
+    public function createSeason(SeasonEntity $entity): void
+    {
+
+    }
+
+    public function updateSeason(SeasonEntity $entity): void
+    {
+
     }
 }
