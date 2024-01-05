cb2b6440fea499cf2893f018cbd528db6bbf806a
Jack Ellis <jackellis1504@gmail.com>
2024-01-05 14:55:33 +0000

    wip

--- src/Framework/Form/CastMemberType.php
+++ src/Framework/Form/CastMemberType.php
@@ -4,12 +4,40 @@ declare(strict_types=1);
 
 namespace App\Framework\Form;
 
+use App\Application\Person\PersonFinderInterface;
+use App\Application\Show\ShowFinderInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class CastMemberType extends AbstractType
 {
+    public function __construct(
+        private readonly PersonFinderInterface $personFinder,
+        private readonly ShowFinderInterface $showFinder,
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder
+            ->add('role', TextType::class)
+            ->add(
+                'person',
+                ChoiceType::class,
+                [
+                    'choices' => $this->personFinder->findAll(),
+                    'choice_label' => 'name',
+                    'choice_value' => 'id',
+                ]
+            )
+            ->add(
+                'submit',
+                SubmitType::class
+            )
+
+        ;
     }
 }
--- src/Framework/Form/CrewMemberType.php
+++ src/Framework/Form/CrewMemberType.php
@@ -4,12 +4,45 @@ declare(strict_types=1);
 
 namespace App\Framework\Form;
 
+use App\Application\CrewRole\CrewRoleFinderInterface;
+use App\Application\Person\PersonFinderInterface;
+use App\Application\Show\ShowFinderInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class CrewMemberType extends AbstractType
 {
+    public function __construct(
+        private readonly CrewRoleFinderInterface $crewRoleFinder,
+        private readonly PersonFinderInterface $personFinder,
+        private readonly ShowFinderInterface $showFinder,
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder
+            ->add('role', ChoiceType::class, [
+                'choices' => $this->crewRoleFinder->findAll(),
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+            ])
+            ->add('person', ChoiceType::class, [
+                'choices' => $this->personFinder->findAll(),
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+            ])
+            ->add('show', ChoiceType::class, [
+                'choices' => $this->showFinder->findAll(),
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+            ])
+            ->add(
+                'submit',
+                SubmitType::class
+            )
+        ;
     }
 }
