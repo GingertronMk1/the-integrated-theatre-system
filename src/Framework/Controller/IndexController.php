f1ac85595240440e9920489dc0eed5aedfb52a56
Jack Ellis <jackellis1504@gmail.com>
2024-01-09 18:41:27 +0000

    Update index

--- src/Framework/Controller/IndexController.php
+++ src/Framework/Controller/IndexController.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Framework\Controller;
 
+use App\Application\Person\PersonFinderInterface;
+use App\Application\Season\SeasonFinderInterface;
+use App\Application\Show\ShowFinderInterface;
+use App\Application\TrainingCategory\TrainingCategoryFinderInterface;
+use App\Application\TrainingItem\TrainingItemFinderInterface;
+use App\Application\TrainingSession\TrainingSessionFinderInterface;
 use App\Application\User\UserFinderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +18,26 @@ use Symfony\Component\Routing\Attribute\Route;
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(UserFinderInterface $userFinder): Response
+    public function index(
+        UserFinderInterface $userFinder,
+        ShowFinderInterface $showFinder,
+        SeasonFinderInterface $seasonFinder,
+        PersonFinderInterface $personFinder,
+        TrainingCategoryFinderInterface $trainingCategoryFinder,
+        TrainingItemFinderInterface $trainingItemFinder,
+        TrainingSessionFinderInterface $trainingSessionFinder
+    ): Response
     {
-        return $this->render('pages/index/index.html.twig', ['users' => $userFinder->findAll()]);
+        return $this->render(
+            'pages/index/index.html.twig',
+            [
+                'shows' => $showFinder->findAll(),
+                'seasons' => $seasonFinder->findAll(),
+                'people' => $personFinder->findAll(),
+                'trainingCategories' => $trainingCategoryFinder->findAll(),
+                'trainingItems' => $trainingItemFinder->findAll(),
+                'trainingSessions' => $trainingSessionFinder->findAll()
+            ]
+        );
     }
 }
