a9cbb8525ae5bbd44555dddfc6a28c71cf209868
Jack Ellis <jackellis1504@gmail.com>
2024-07-03 12:53:52 +0100

    soft deletes and timestamps
     M app/Models/User.php
     M database/migrations/0001_01_01_000000_create_users_table.php
     M stubs/migration.create.stub
     M stubs/model.stub

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasUuids, Notifiable;
+    use HasFactory, HasUuids, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
