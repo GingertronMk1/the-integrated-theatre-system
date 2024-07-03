1e1bbb9eae95d9d1a54a6646f75abf8807171077
Jack Ellis <jackellis1504@gmail.com>
2024-07-03 12:57:20 +0100

    Adding people

---
+++ app/Models/Person.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+}
