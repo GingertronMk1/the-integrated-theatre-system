1279576104c20db226270660110f144c7e5367ee
Jack Ellis <jackellis1504@gmail.com>
2024-07-03 12:12:37 +0100

    wip
    ?? .editorconfig
    ?? .env.example
    ?? .env.local
    ?? .gitattributes
    ?? .gitignore
    ?? _docker/
    ?? app/
    ?? artisan
    ?? bin/
    ?? bootstrap/
    ?? composer.json
    ?? composer.lock
    ?? config/
    ?? database/
    ?? docker-compose.yml
    ?? package-lock.json
    ?? package.json
    ?? phpunit.xml
    ?? postcss.config.js
    ?? public/
    ?? resources/
    ?? routes/
    ?? storage/
    ?? tailwind.config.js
    ?? tests/
    ?? vite.config.js

---
+++ _docker/plugins/default-login.php
@@ -0,0 +1,58 @@
+<?php
+
+class DefaultLogin
+{
+    /** Print login form.
+     */
+    public function loginForm()
+    {
+        echo '<p hidden data-plugin="default-login">Plugin on</p>';
+        echo "<table cellspacing='0' class='layout'>";
+        echo $this->loginFormField(
+            'driver',
+            '',
+            'sqlite'
+        );
+        echo $this->loginFormField(
+            'username',
+            '',
+            'admin'
+        );
+        echo $this->loginFormField(
+            'password',
+            '',
+            'admin'
+        );
+        echo $this->loginFormField(
+            'db',
+            'DB_DATABASE',
+        );
+        echo '</table>';
+        echo "<p><input type='submit' value='Login'>";
+
+        return 0;
+    }
+
+    public function loginFormField(string $name, string $envValue = '', string $value = ''): string
+    {
+        $inputValue = $value;
+        if (!empty($envValue) && isset($_ENV[$envValue])) {
+            $inputValue = $_ENV[$envValue];
+        }
+
+        $nameAttr = "auth[{$name}]";
+
+        return <<<HTML
+            <tr>
+                <td>
+                    {$name}
+                </td>
+                <td>
+                    <input name="{$nameAttr}" value="{$inputValue}" />
+                </td>
+            </tr>
+        HTML;
+    }
+}
+
+return new DefaultLogin();
