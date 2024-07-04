dfbca0ea65e26b7f0834ac2dbd60fc7d0f51370c
Jack Ellis <jackellis1504@gmail.com>
2024-07-04 11:25:20 +0100

    Make people index auth'ed

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resources([
+        'person' => PersonController::class,
+    ]);
 });
-Route::resources([
-    'person' => PersonController::class,
-]);
 
 require __DIR__.'/auth.php';
