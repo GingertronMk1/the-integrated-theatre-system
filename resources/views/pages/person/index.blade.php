5da2ba7ba476a2735845757df3da361613689532
Jack Ellis <jackellis1504@gmail.com>
2024-07-04 14:22:06 +0100

    updates to vite config
     M .env.example
     M resources/views/pages/person/index.blade.php
     M vite.config.js

--- resources/views/pages/person/index.blade.php
+++ resources/views/pages/person/index.blade.php
@@ -1,14 +1,31 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('People') }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
         <a href="{{ route('person.create') }}">Add</a>
-        <table>
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>User's Name</th>
+                    <th>Start Year</th>
+                    <th>End Year</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
             @foreach ($people as $person)
                 <tr data-person-id="{{ $person->id }}">
                     <td>{{ $person->name }}</td>
                     <td>{{ $person->user?->name ?? 'No user associated' }}</td>
-                    <td>{{ $person->start_year ?? 'Start Year unknown' }}</td>
-                    <td>{{ $person->end_year ?? 'End Year unknown' }}</td>
-                    <td>
+                    <td class="text-center">{{ $person->start_year ?? 'Start Year unknown' }}</td>
+                    <td class="text-center">{{ $person->end_year ?? 'End Year unknown' }}</td>
+                    <td class="flex justify-evenly space-x-2">
                         <a href="{{ route('person.edit', ['person' => $person]) }}">Update</a>
                         <form
                             method="POST"
@@ -21,5 +38,6 @@
                 </tr>
             @endforeach
         </table>
+        </div>
     </div>
 </x-app-layout>
