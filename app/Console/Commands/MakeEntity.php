8a682d3dcbd3fd2d5fb0eeb64b8f658c747a4815
Jack Ellis <jackellis1504@gmail.com>
2024-07-04 14:40:52 +0100

    makeentity command
    ?? app/Console/

---
+++ app/Console/Commands/MakeEntity.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeEntity extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-entity {modelName}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $modelName = $this->argument('modelName');
+        $modelNameLC = lcfirst($modelName);
+
+        $this->call(
+            'make:model',
+            [
+                'name' => $modelName,
+                '--all' => true,
+                '--phpunit' => true
+            ]
+        );
+        foreach([
+            'index',
+            'create',
+            'edit',
+            'show'
+        ] as $viewName) {
+            $this->call(
+                'make:view',
+                [
+                    'name' => "pages/{$modelNameLC}/{$viewName}"
+                ]
+            );
+        }
+    }
+}
