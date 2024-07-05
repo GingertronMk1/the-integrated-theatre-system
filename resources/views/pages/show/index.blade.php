c00d48dc699c9eefbf2c62307dc6ce0ff50023b5
Jack Ellis <jackellis1504@gmail.com>
2024-07-05 17:12:47 +0100

    update show route
     M resources/views/pages/show/create.blade.php
     M resources/views/pages/show/edit.blade.php
     M resources/views/pages/show/index.blade.php

--- resources/views/pages/show/index.blade.php
+++ resources/views/pages/show/index.blade.php
@@ -2,9 +2,13 @@
 
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
+            <a href="{{ route('show.create') }}">Add</a>
             @foreach ($shows as $show)
                 <div class="flex flex-col">
-                    <h3 class="text-xl">{{ $show->title }}</h3>
+                    <span class="flex flex-row justify-between">
+                        <h3 class="text-xl">{{ $show->title }}</h3>
+                        <a href="{{ route('show.edit', ['show' => $show]) }}">Edit</a>
+                    </span>
                     <div class="flex flex-row divide-x-2">
                         <div class="flex-1 px-2">
                             @toparagraphs($show->description)
