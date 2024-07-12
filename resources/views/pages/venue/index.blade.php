612e71014f41a6b7af01db2db85da92df1a36f5e
Jack Ellis <jackellis1504@gmail.com>
2024-07-12 20:01:42 +0100

    fix blade
     M resources/views/pages/venue/index.blade.php

--- resources/views/pages/venue/index.blade.php
+++ resources/views/pages/venue/index.blade.php
@@ -7,29 +7,26 @@
         <i class="fa-solid fa-plus"></i>
         Add
     </a>
-    @foreach($venues as $venue)
-    <div class="card mt-3">
-        <div class="card-header">{{ $venue->name }}</div>
+    @foreach ($venues as $venue)
+        <div class="card mt-3">
+            <div class="card-header">{{ $venue->name }}</div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">
                     {{ $venue->location }}
                 </li>
-                @if($venue->location_additional)
+                @if ($venue->location_additional)
                     <li class="list-group-item">
                         {{ $venue->location_additional }}
                     </li>
                 @endif
-                <li
-                    class="list-group-item"
-                    x-data="{capacity: {{ $venue->capacity }}}"
-                    x-text="`Capacity: ${capacity.toLocaleString()}`">
+                <li class="list-group-item" x-data="{ capacity: {{ $venue->capacity }} }" x-text="`Capacity: ${capacity.toLocaleString()}`">
                 </li>
             </ul>
-        <div class="card-footer">
-            <a href="{{ route('venue.edit', ['venue' => $venue]) }}" class="btn btn-primary">
-                Edit
-            </a>
+            <div class="card-footer">
+                <a href="{{ route('venue.edit', ['venue' => $venue]) }}" class="btn btn-primary">
+                    Edit
+                </a>
+            </div>
         </div>
-    </div>
     @endforeach
 </x-app-layout>
