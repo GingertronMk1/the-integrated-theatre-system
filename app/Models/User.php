a0b0c3a358fa765aa467bf77d5e8dd86a65fd7a4
Jack Ellis <jackellis1504@gmail.com>
2025-03-11 11:54:21 +0000

    default soft deletes
     M app/Models/User.php
     M stubs/migration.create.stub
     M stubs/model.stub

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,16 @@ namespace App\Models;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
