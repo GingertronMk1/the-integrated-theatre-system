e2de8d9eb22bd9fd278c826316f7e6de286c2b94
Jack Ellis <jackellis1504@gmail.com>
2025-03-11 14:45:16 +0000

    fix pinting
     M app/Models/User.php
     M composer.json

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use Notifiable;
     use SoftDeletes;
 
