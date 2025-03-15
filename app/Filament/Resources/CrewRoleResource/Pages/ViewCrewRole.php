718a78168b7a3bcbd83f253cd168de419bdb4c77
Jack Ellis <jackellis1504@gmail.com>
2025-03-15 17:29:25 +0000

    add an action to replace a crew role in all member instances
     M app/Filament/Resources/CrewRoleResource/Pages/ViewCrewRole.php

--- app/Filament/Resources/CrewRoleResource/Pages/ViewCrewRole.php
+++ app/Filament/Resources/CrewRoleResource/Pages/ViewCrewRole.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\CrewRoleResource\Pages;
 
 use App\Filament\Resources\CrewRoleResource;
+use App\Models\CrewMember;
+use App\Models\CrewRole;
 use Filament\Actions;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewCrewRole extends ViewRecord
@@ -12,8 +15,34 @@ class ViewCrewRole extends ViewRecord
 
     protected function getHeaderActions(): array
     {
+        $idKey = 'crew_role_id';
+
         return [
             Actions\EditAction::make(),
+            Actions\Action::make('replace')
+                ->form([
+                    Select::make($idKey)
+                        ->label('New Role')
+                        ->options(CrewRole::query()->pluck('name', 'id'))
+                        ->searchable()
+                        ->required(),
+                ])
+                ->action(function (array $data, CrewRole $record) use ($idKey): void {
+                    $newId = $data[$idKey];
+                    CrewMember::query()
+                        ->where(
+                            'crew_role_id',
+                            '=',
+                            $record->id
+                        )
+                        ->update(['crew_role_id' => $newId]);
+                    $record->delete();
+                    session()->flash('status', 'Role successfully replaced');
+                    $this->redirectRoute(
+                        'filament.admin.resources.crew-roles.view',
+                        ['record' => $newId]
+                    );
+                }),
         ];
     }
 }
