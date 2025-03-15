b65c26135ea7a347b829f6686c10062a84014972
Jack Ellis <jackellis1504@gmail.com>
2025-03-15 15:15:07 +0000

    Okay lets have admin in here

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -55,6 +55,12 @@ function handleImageError() {
 
                         <template v-else>
                             <Link
+                                :href="route('filament.admin.pages.dashboard')"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Admin
+                            </Link>
+                            <Link
                                 :href="route('login')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
