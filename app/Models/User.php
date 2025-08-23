aac9fff103a92100bbac2bd8a44e385ac4d1f3b7
Jack Ellis <jackellis1504@gmail.com>
2025-08-23 14:57:23 +0100

    wip
     M app/Models/User.php
     M pint.json

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use HasUuids;
     use Notifiable;
     use SoftDeletes;
