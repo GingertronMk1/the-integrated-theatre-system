3e7ff5960d9379e5c4a2b39fc575722f59a99b1c
Jack Ellis <jackellis1504@gmail.com>
2025-08-23 14:37:18 +0100

    add person
    ?? app/Http/Controllers/PersonController.php
    ?? app/Http/Requests/StorePersonRequest.php
    ?? app/Http/Requests/UpdatePersonRequest.php
    ?? app/Models/Person.php
    ?? app/Policies/
    ?? database/factories/PersonFactory.php
    ?? database/migrations/2025_08_23_133456_create_people_table.php
    ?? database/seeders/PersonSeeder.php

---
+++ app/Models/Person.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    /** @use HasFactory<\Database\Factories\PersonFactory> */
+    use HasFactory;
+    use HasUuids;
+    use SoftDeletes;
+}
